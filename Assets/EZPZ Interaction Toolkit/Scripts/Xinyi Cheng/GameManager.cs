1077e48201b1b2926ee2edc78381b35f8c3da59f
chengxinyiiiiii-byte <chengxinyiiiiii@gmail.com>
2026-04-12 18:23:43 +1000

    improve

--- Assets/EZPZ Interaction Toolkit/Scripts/Xinyi Cheng/GameManager.cs
+++ Assets/EZPZ Interaction Toolkit/Scripts/Xinyi Cheng/GameManager.cs
@@ -12,17 +12,20 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        boardText.text =
-            "Mia: I'm really hungry...\n\n" +
-            "Leo: Just a small piece is fine.\n" +
-            "Cecilia: Me too, just a little is enough.\n\n" +
-            "Don't forget yourself.\n\n" +
-            "Press SPACE to check.";
+        if (boardText != null)
+        {
+            boardText.text =
+                "Mia: I'm really hungry...\n\n" +
+                "Leo: Just a small piece is fine.\n" +
+                "Cecilia: Me too, just a little is enough.\n\n" +
+                "Don't forget yourself.\n\n" +
+                "Have you shared fairly? Press F to find out.";
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             CheckResult();
         }
@@ -30,6 +33,12 @@ public class GameManager : MonoBehaviour
 
     void CheckResult()
     {
+        if (plateMia == null || plateLeo == null || plateCecilia == null || platePlayer == null || boardText == null)
+        {
+            Debug.LogError("GameManager is missing references. Please assign all plate counters and boardText in the Inspector.");
+            return;
+        }
+
         if (plateMia.cupcakeCount == 3 &&
             plateLeo.cupcakeCount == 1 &&
             plateCecilia.cupcakeCount == 1 &&
@@ -39,53 +48,77 @@ public class GameManager : MonoBehaviour
                 "Mia: Thank you! That helps a lot.\n\n" +
                 "Leo: Perfect for me.\n" +
                 "Cecilia: That's just right.\n\n" +
-                "You: I deserve a piece too.";
+                "You: I have my share too.";
         }
         else
         {
-            string miaFeedback = "";
-            string leoFeedback = "";
-            string ceciliaFeedback = "";
-            string playerFeedback = "";
+            string miaFeedback;
+            string leoFeedback;
+            string ceciliaFeedback;
+            string playerFeedback;
 
             // Mia feedback
             if (plateMia.cupcakeCount < 3)
+            {
                 miaFeedback = "Mia: I'm still hungry...";
+            }
             else if (plateMia.cupcakeCount > 3)
+            {
                 miaFeedback = "Mia: That's more than I need.";
+            }
             else
+            {
                 miaFeedback = "Mia: Mine is fine.";
+            }
 
             // Leo feedback
             if (plateLeo.cupcakeCount == 0)
+            {
                 leoFeedback = "Leo: I didn't get any.";
+            }
             else if (plateLeo.cupcakeCount == 1)
+            {
                 leoFeedback = "Leo: Mine is fine.";
+            }
             else
+            {
                 leoFeedback = "Leo: That's too much for me.";
+            }
 
             // Cecilia feedback
             if (plateCecilia.cupcakeCount == 0)
+            {
                 ceciliaFeedback = "Cecilia: I didn't get any.";
+            }
             else if (plateCecilia.cupcakeCount == 1)
+            {
                 ceciliaFeedback = "Cecilia: Mine is fine.";
+            }
             else
+            {
                 ceciliaFeedback = "Cecilia: That's too much for me.";
+            }
 
             // Player feedback
             if (platePlayer.cupcakeCount == 0)
+            {
                 playerFeedback = "You: Wait... did I forget myself?";
+            }
             else if (platePlayer.cupcakeCount == 1)
+            {
                 playerFeedback = "You: I have my share.";
+            }
             else
+            {
                 playerFeedback = "You: That's more than I need.";
+            }
 
             boardText.text =
                 miaFeedback + "\n\n" +
                 leoFeedback + "\n" +
                 ceciliaFeedback + "\n\n" +
                 playerFeedback + "\n\n" +
-                "Try again.";
+                "Try again. Press F to recheck.";
         }
     }
 }
